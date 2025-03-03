87a0b816b73c4084cb1b79368357063f96c4905b
lizhifu <lizhifuabc@163.com>
2025-03-03 14:38:48 +0800

    feat(rbac): 添加跨域配置
    - 在 application.properties 中添加 access-control-allow-origin 属性，允许所有源跨域访问
    - 新增 CorsFilterConfig 类，配置跨域过滤器
    - 设置跨域请求的源、头、方法等

---
+++ component-rbac/src/main/java/io/github/lizhifuabc/rbac/config/CorsFilterConfig.java
@@ -0,0 +1,40 @@
+package io.github.lizhifuabc.rbac.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * 跨域配置
+ *
+ * @author lizhifu
+ * @since 2024/2/24
+ */
+@Configuration
+public class CorsFilterConfig {
+
+    @Value("${access-control-allow-origin:*}")
+    private String accessControlAllowOrigin;
+    
+    /**
+     * 跨域配置
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // 设置访问源地址
+        config.addAllowedOriginPattern(accessControlAllowOrigin);
+        // 设置访问源请求头
+        config.addAllowedHeader("*");
+        // 设置访问源请求方法
+        config.addAllowedMethod("*");
+        // 对接口配置跨域设置
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
