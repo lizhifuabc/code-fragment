7d25c08eb849cd96981af1cfc8cf7c94d54829fc
lizhifu <lizhifuabc@163.com>
2025-03-04 17:20:04 +0800

    feat(component): 添加 spring-boot-log模块
    - 新增 spring-boot-log 模块，用于日志记录和监控
    - 添加日志配置文件 application.properties
    - 创建 Spring Boot 应用启动类 SpringBootLogApplication
    - 配置 pom.xml 文件，添加所需依赖和插件

---
+++ spring-boot-log/src/main/java/io/github/lizhifuabc/log/SpringBootLogApplication.java
@@ -0,0 +1,17 @@
+package io.github.lizhifuabc.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot 日志应用启动类
+ *
+ * @author lizhifu
+ * @since 2024/2/25
+ */
+@SpringBootApplication
+public class SpringBootLogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLogApplication.class, args);
+    }
+}
