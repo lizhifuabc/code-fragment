5cd000f8ce77c19f5ef4a7892fe6a44bc3d94002
lizhifu <lizhifuabc@163.com>
2025-03-17 09:54:33 +0800

    refactor(component-rbac): 优化数据库表结构定义

--- component-rbac/db/init.sql
+++ component-rbac/db/init.sql
@@ -14,7 +14,7 @@ use component_rbac;
 -- ----------------------------
 create table sys_tenant
 (
-    tenant_id         bigint(20)    not null        comment '租户id',
+    tenant_id         bigint    not null        comment '租户id',
     contact_user_name varchar(20)                   comment '联系人',
     contact_phone     varchar(20)                   comment '联系电话',
     company_name      varchar(30)                   comment '企业名称',
@@ -27,10 +27,10 @@ create table sys_tenant
     account_count     int           default -1      comment '用户数量（-1不限制）',
     status            char(1)       default '0'     comment '租户状态（0正常 1停用）',
     del_flag          char(1)       default '0'     comment '删除标志（0代表存在 1代表删除）',
-    create_dept       bigint(20)                    comment '创建部门',
-    create_by         bigint(20)                    comment '创建者',
+    create_dept       bigint                    comment '创建部门',
+    create_by         bigint                    comment '创建者',
     create_time       datetime      default current_timestamp  comment '创建时间',
-    update_by         bigint(20)                    comment '更新者',
+    update_by         bigint                    comment '更新者',
     update_time       datetime                      comment '更新时间',
     primary key (tenant_id)
 ) engine=innodb comment = '租户表';
@@ -39,16 +39,16 @@ create table sys_tenant
 -- 租户套餐表
 -- ----------------------------
 create table sys_tenant_package (
-    tenant_package_id       bigint(20)     not null    comment '租户套餐id',
+    tenant_package_id       bigint     not null    comment '租户套餐id',
     package_name            varchar(20)                comment '套餐名称',
     remark                  varchar(200)               comment '备注',
-    menu_check_strictly     tinyint(1)     default 1   comment '菜单树选择项是否关联显示',
+    menu_check_strictly     tinyint     default 1   comment '菜单树选择项是否关联显示',
     status                  char(1)        default '0' comment '状态（0正常 1停用）',
     del_flag                char(1)        default '0' comment '删除标志（0代表存在 1代表删除）',
-    create_dept             bigint(20)                 comment '创建部门',
-    create_by               bigint(20)                 comment '创建者',
+    create_dept             bigint                 comment '创建部门',
+    create_by               bigint                 comment '创建者',
     create_time             datetime        default current_timestamp  comment '创建时间',
-    update_by               bigint(20)                 comment '更新者',
+    update_by               bigint                 comment '更新者',
     update_time             datetime                   comment '更新时间',
     primary key (tenant_package_id)
 ) engine=innodb comment = '租户套餐表';
@@ -57,8 +57,8 @@ create table sys_tenant_package (
 -- 租户套餐和菜单关联表  租户套餐1-N菜单
 -- ----------------------------
 create table sys_tenant_package_menu (
-   tenant_package_id   bigint(20) not null comment '租户套餐ID',
-   menu_id   bigint(20) not null comment '菜单ID',
+   tenant_package_id   bigint not null comment '租户套餐ID',
+   menu_id   bigint not null comment '菜单ID',
    primary key(tenant_package_id,menu_id)
 ) engine=innodb comment = '租户套餐和菜单关联表';
 
@@ -67,8 +67,8 @@ create table sys_tenant_package_menu (
 -- 租户和租户套餐关联表  租户1-N租户套餐
 -- ----------------------------
 create table sys_tenant_package_ref (
-    tenant_package_id   bigint(20) not null comment '租户套餐ID',
-    tenant_id   bigint(20) not null comment '租户ID',
+    tenant_package_id   bigint not null comment '租户套餐ID',
+    tenant_id   bigint not null comment '租户ID',
     primary key(tenant_package_id,tenant_id)
 ) engine=innodb comment = '租户和租户套餐关联表';
 
@@ -76,22 +76,22 @@ create table sys_tenant_package_ref (
 -- 部门表
 -- ----------------------------
 create table sys_dept (
-    dept_id           bigint(20)      not null                   comment '部门id',
-    tenant_id         bigint(20)      not null                   comment '租户id',
-    parent_id         bigint(20)      default 0                  comment '父部门id',
+    dept_id           bigint      not null                   comment '部门id',
+    tenant_id         bigint      not null                   comment '租户id',
+    parent_id         bigint      default 0                  comment '父部门id',
     ancestors         varchar(500)    default ''                 comment '祖级列表',
     dept_name         varchar(30)     default ''                 comment '部门名称',
     dept_category     varchar(100)    default null               comment '部门类别编码',
-    order_num         int(4)          default 0                  comment '显示顺序',
-    leader            bigint(20)      default null               comment '负责人',
+    order_num         int          default 0                  comment '显示顺序',
+    leader            bigint      default null               comment '负责人',
     phone             varchar(11)     default null               comment '联系电话',
     email             varchar(50)     default null               comment '邮箱',
     status            char(1)         default '0'                comment '部门状态（0正常 1停用）',
     del_flag          char(1)         default '0'                comment '删除标志（0代表存在 1代表删除）',
-    create_dept       bigint(20)      default null               comment '创建部门',
-    create_by         bigint(20)      default null               comment '创建者',
+    create_dept       bigint      default null               comment '创建部门',
+    create_by         bigint      default null               comment '创建者',
     create_time       datetime        default current_timestamp  comment '创建时间',
-    update_by         bigint(20)      default null               comment '更新者',
+    update_by         bigint      default null               comment '更新者',
     update_time       datetime                                   comment '更新时间',
     primary key (dept_id)
 ) engine=innodb comment = '部门表';
@@ -100,25 +100,25 @@ create table sys_dept (
 -- 用户信息表
 -- ----------------------------
 create table sys_user (
-    user_id           bigint(20)      not null                   comment '用户ID',
-    tenant_id         bigint(20)      not null                   comment '租户id',
-    dept_id           bigint(20)      default null               comment '部门ID',
+    user_id           bigint      not null                   comment '用户ID',
+    tenant_id         bigint      not null                   comment '租户id',
+    dept_id           bigint      default null               comment '部门ID',
     user_name         varchar(30)     not null                   comment '用户账号',
     nick_name         varchar(30)     not null                   comment '用户昵称',
     user_type         varchar(10)     default 'sys_user'         comment '用户类型（sys_user系统用户）',
     email             varchar(50)     default ''                 comment '用户邮箱',
     phone             varchar(11)     default ''                 comment '手机号码',
     sex               char(1)         default '0'                comment '用户性别（0男 1女 2未知）',
-    avatar            bigint(20)                                 comment '头像地址',
+    avatar            bigint                                 comment '头像地址',
     password          varchar(100)    default ''                 comment '密码',
     status            char(1)         default '0'                comment '帐号状态（0正常 1停用）',
     del_flag          char(1)         default '0'                comment '删除标志（0代表存在 1代表删除）',
     login_ip          varchar(128)    default ''                 comment '最后登录IP',
     login_date        datetime                                   comment '最后登录时间',
-    create_dept       bigint(20)      default null               comment '创建部门',
-    create_by         bigint(20)      default null               comment '创建者',
+    create_dept       bigint      default null               comment '创建部门',
+    create_by         bigint      default null               comment '创建者',
     create_time       datetime        default current_timestamp  comment '创建时间',
-    update_by         bigint(20)      default null               comment '更新者',
+    update_by         bigint      default null               comment '更新者',
     update_time       datetime                                   comment '更新时间',
     remark            varchar(500)    default null               comment '备注',
     primary key (user_id)
@@ -129,18 +129,18 @@ create table sys_user (
 -- ----------------------------
 create table sys_post
 (
-    post_id       bigint(20)      not null                   comment '岗位ID',
-    tenant_id     bigint(20)      not null                   comment '租户id',
-    dept_id       bigint(20)      not null                   comment '部门id',
+    post_id       bigint      not null                   comment '岗位ID',
+    tenant_id     bigint      not null                   comment '租户id',
+    dept_id       bigint      not null                   comment '部门id',
     post_code     varchar(64)     not null                   comment '岗位编码',
     post_category varchar(100)    default null               comment '岗位类别编码',
     post_name     varchar(50)     not null                   comment '岗位名称',
-    post_sort     int(4)          not null                   comment '显示顺序',
+    post_sort     int          not null                   comment '显示顺序',
     status        char(1)         not null                   comment '状态（0正常 1停用）',
-    create_dept   bigint(20)      default null               comment '创建部门',
-    create_by     bigint(20)      default null               comment '创建者',
+    create_dept   bigint      default null               comment '创建部门',
+    create_by     bigint      default null               comment '创建者',
     create_time   datetime        default current_timestamp  comment '创建时间',
-    update_by     bigint(20)      default null               comment '更新者',
+    update_by     bigint      default null               comment '更新者',
     update_time   datetime                                   comment '更新时间',
     remark        varchar(500)    default null               comment '备注',
     primary key (post_id)
@@ -150,20 +150,20 @@ create table sys_post
 -- 角色信息表
 -- ----------------------------
 create table sys_role (
-    role_id              bigint(20)      not null                   comment '角色ID',
-    tenant_id            bigint(20)      not null                   comment '租户id',
+    role_id              bigint      not null                   comment '角色ID',
+    tenant_id            bigint      not null                   comment '租户id',
     role_name            varchar(30)     not null                   comment '角色名称',
     role_key             varchar(100)    not null                   comment '角色权限字符串',
-    role_sort            int(4)          not null                   comment '显示顺序',
+    role_sort            int          not null                   comment '显示顺序',
     data_scope           char(1)         default '1'                comment '数据范围（1：全部数据权限 2：自定数据权限 3：本部门数据权限 4：本部门及以下数据权限 5：仅本人数据权限 6：部门及以下或本人数据权限）',
-    menu_check_strictly  tinyint(1)      default 1                  comment '菜单树选择项是否关联显示',
-    dept_check_strictly  tinyint(1)      default 1                  comment '部门树选择项是否关联显示',
+    menu_check_strictly  tinyint      default 1                  comment '菜单树选择项是否关联显示',
+    dept_check_strictly  tinyint      default 1                  comment '部门树选择项是否关联显示',
     status               char(1)         not null                   comment '角色状态（0正常 1停用）',
     del_flag             char(1)         default '0'                comment '删除标志（0代表存在 1代表删除）',
-    create_dept          bigint(20)      default null               comment '创建部门',
-    create_by            bigint(20)      default null               comment '创建者',
+    create_dept          bigint      default null               comment '创建部门',
+    create_by            bigint      default null               comment '创建者',
     create_time          datetime        default current_timestamp  comment '创建时间',
-    update_by            bigint(20)      default null               comment '更新者',
+    update_by            bigint      default null               comment '更新者',
     update_time          datetime                                   comment '更新时间',
     remark               varchar(500)    default null               comment '备注',
     primary key (role_id)
@@ -173,10 +173,10 @@ create table sys_role (
 -- 菜单权限表
 -- ----------------------------
 create table sys_menu (
-    menu_id           bigint(20)      not null                   comment '菜单ID',
+    menu_id           bigint      not null                   comment '菜单ID',
     menu_name         varchar(50)     not null                   comment '菜单名称',
-    parent_id         bigint(20)      default 0                  comment '父菜单ID',
-    order_num         int(4)          default 0                  comment '显示顺序',
+    parent_id         bigint      default 0                  comment '父菜单ID',
+    order_num         int          default 0                  comment '显示顺序',
     path              varchar(200)    default ''                 comment '路由地址',
     component         varchar(255)    default null               comment '组件路径',
     query_param       varchar(255)    default null               comment '路由参数',
@@ -187,10 +187,10 @@ create table sys_menu (
     status            char(1)         default 0                  comment '菜单状态（0正常 1停用）',
     perms             varchar(100)    default null               comment '权限标识',
     icon              varchar(100)    default '#'                comment '菜单图标',
-    create_dept       bigint(20)      default null               comment '创建部门',
-    create_by         bigint(20)      default null               comment '创建者',
+    create_dept       bigint      default null               comment '创建部门',
+    create_by         bigint      default null               comment '创建者',
     create_time       datetime        default current_timestamp  comment '创建时间',
-    update_by         bigint(20)      default null               comment '更新者',
+    update_by         bigint      default null               comment '更新者',
     update_time       datetime                                   comment '更新时间',
     remark            varchar(500)    default ''                 comment '备注',
     primary key (menu_id)
@@ -201,9 +201,9 @@ create table sys_menu (
 -- 用户和角色关联表  用户N-1角色
 -- ----------------------------
 create table sys_user_role (
-    user_id   bigint(20) not null comment '用户ID',
-    role_id   bigint(20) not null comment '角色ID',
-    tenant_id         bigint(20)      not null                   comment '租户id',
+    user_id   bigint not null comment '用户ID',
+    role_id   bigint not null comment '角色ID',
+    tenant_id         bigint      not null                   comment '租户id',
     primary key(tenant_id,user_id, role_id)
 ) engine=innodb comment = '用户和角色关联表';
 
@@ -211,9 +211,9 @@ create table sys_user_role (
 -- 角色和菜单关联表  角色1-N菜单
 -- ----------------------------
 create table sys_role_menu (
-    role_id   bigint(20) not null comment '角色ID',
-    menu_id   bigint(20) not null comment '菜单ID',
-    tenant_id         bigint(20)      not null                   comment '租户id',
+    role_id   bigint not null comment '角色ID',
+    menu_id   bigint not null comment '菜单ID',
+    tenant_id         bigint      not null                   comment '租户id',
     primary key(tenant_id,role_id, menu_id)
 ) engine=innodb comment = '角色和菜单关联表';
 
@@ -222,9 +222,9 @@ create table sys_role_menu (
 -- 角色和部门关联表  角色1-N部门
 -- ----------------------------
 create table sys_role_dept (
-    role_id   bigint(20) not null comment '角色ID',
-    dept_id   bigint(20) not null comment '部门ID',
-    tenant_id         bigint(20)      not null                   comment '租户id',
+    role_id   bigint not null comment '角色ID',
+    dept_id   bigint not null comment '部门ID',
+    tenant_id         bigint      not null                   comment '租户id',
     primary key(tenant_id,role_id, dept_id)
 ) engine=innodb comment = '角色和部门关联表';
 
@@ -233,8 +233,8 @@ create table sys_role_dept (
 -- ----------------------------
 create table sys_user_post
 (
-    user_id   bigint(20) not null comment '用户ID',
-    post_id   bigint(20) not null comment '岗位ID',
-    tenant_id         bigint(20)      not null                   comment '租户id',
+    user_id   bigint not null comment '用户ID',
+    post_id   bigint not null comment '岗位ID',
+    tenant_id         bigint      not null                   comment '租户id',
     primary key (tenant_id,user_id, post_id)
 ) engine=innodb comment = '用户与岗位关联表';
